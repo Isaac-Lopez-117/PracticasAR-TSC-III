9c140191196c9e3504e816aac213b5865c11b5b8
Isaac-Lopez-117 <IsaacLopez117@hotmail.com>
2023-05-20 16:20:07 -0600

    Cambio de niveles

--- Assets/Script/LevelManager.cs
+++ Assets/Script/LevelManager.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     public int intentos;
+
+    GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager.controlNiveles();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/Script/goal.cs
+++ Assets/Script/goal.cs
@@ -6,21 +6,22 @@ public class goal : MonoBehaviour
 {
     public GameManager gameManager;
     
-
     private void OnTriggerEnter(Collider collider)
     {
         // Verifica si el objeto ha llegado a la meta
         if (collider.gameObject.name == "Pelota")
         {
-            // Cambiar el color del objeto
-            //collider.gameObject.GetComponent<Renderer>().material.color = goalColor;
             gameManager.indiceNivel += 1;
-            if (gameManager.indiceNivel > 3)
+            if (gameManager.indiceNivel > 2)
             {
                 gameManager.indiceNivel = 0;
             }
-            
+            Destroy(collider.gameObject);
 
+            if (gameManager != null)
+            {
+                gameManager.controlNiveles();
+            }
         }
     }
 }
